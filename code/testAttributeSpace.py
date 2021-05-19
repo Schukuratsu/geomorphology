74ad07903dd40be504162881189a80a2c0f51fae
Lucas Ennes <lucas.ennes@upperds.com>
2021-05-19 00:54:26 -0300

    refactor e muda plot para seaborn

--- code/testAttributeSpace.py
+++ code/testAttributeSpace.py
@@ -23,37 +23,6 @@ def laplacian8(mat):
     return filteredMat
 
 
-def plotHist(arr, bins=None, limits=None, show=True):
-    if not limits:
-        # avoid the -1 on finding the min value
-        maxValue = np.max(arr)
-        tempArr = np.copy(arr)
-        tempArr[tempArr == -1] = maxValue
-        minValue = np.min(tempArr)
-        limits = [minValue, maxValue]
-    if not bins:
-        bins = limits[1] - limits[0]
-    hist, bins, patches = plt.hist(arr.ravel(), bins, limits)
-    if show:
-        plt.show()
-    return hist
-
-
-def showImg(mat, console=True):
-    if console:
-        print(mat)
-    absMat = cv2.convertScaleAbs(mat)
-    cv2.imshow("test", absMat)
-    cv2.waitKey(0)
-
-
-def getSimpleRadius(mat, center, nullValue=-1):
-    yi, xi = center
-    for x in range(xi, -1, -1):
-        if mat[yi, x] == nullValue:
-            return xi - x
-
-
 def main():
 
     # inicialização
@@ -67,7 +36,7 @@ def main():
 
     # seleciona a seed
     seeds = pd.read_csv(seedsFileName)
-    seedIndex = 30
+    seedIndex = 31
     seed = {
         "Y": int(seeds.values[seedIndex, 4]),
         "X": int(seeds.values[seedIndex, 3]),
@@ -84,22 +53,8 @@ def main():
     band2 = laplacian4(band1)
     bands = [band1, band2]
 
-    # inicializa matriz de resultado
-    # banda 1 = x
-    # banda 2 = y
-    band1map = demHeightMap
-    band2map = laplacian4(band1map)
-    # xlimits = [int(np.floor(np.min(band1map))), int(np.ceil(np.max(band1map)))]
-    # ylimits = [int(np.floor(np.min(band2map))), int(np.ceil(np.max(band2map)))]
-    xlimits = [int(np.floor(np.min(band1))), int(np.ceil(np.max(band1)))]
-    ylimits = [int(np.floor(np.min(band2))), int(np.ceil(np.max(band2)))]
-    rows = ylimits[1] - ylimits[0]
-    cols = xlimits[1] - xlimits[0]
-    result = np.empty((rows, cols), dtype=np.uint32)
-    print("xlimits: ", xlimits)
-    print("ylimits: ", ylimits)
-    print("rows: ", rows)
-    print("cols: ", cols)
+    # inicializa dataframe
+    result = pd.DataFrame()
 
     # itera sobre os pixels de ambas as bandas ao mesmo tempo
     print("gerando espaço de atributos")
@@ -107,23 +62,14 @@ def main():
         for x in range(band1.shape[1]):
 
             # incrementa elemento da matriz de resultado baseado no valor das bandas naquele pixel
-            resX = int(np.floor(bands[0][y, x] - xlimits[0]) - 1)
-            resY = int(np.floor(bands[1][y, x] - ylimits[0]) - 1)
-            # print(resY, resX)
-            result[resY, resX] = int(result[resY, resX] + 1)
-            # print(result[resY, resX])
+            result = result.append([[bands[0][y, x], bands[1][y, x]]], True)
 
     # gera imagem da matriz de resultado
-    normalizedImg = np.empty_like(result, dtype=np.float32)
-    normalizedImg = cv2.normalize(
-        np.array(result, dtype=np.float32), normalizedImg, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
-    )
-    resizedImg = cv2.resize(normalizedImg, (1500, 1000))
-    cv2.imshow("teste", resizedImg)
-    # plt.hist(result, 255)
-    plt.hist(result)
+    print(result)
+    sns.displot(result, x=0, y=1, binwidth=(10,1))
+    plt.xlim((300,800))
+    plt.ylim((-20,20))
     plt.show()
-    cv2.waitKey(0)
 
     # limpa cache
     if dem:
